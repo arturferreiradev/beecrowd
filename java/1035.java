f01b26a4ec66574f74ca827fd533cd62725f7326
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-04 23:05:40 -0300

    Resolução 1035.java

---
+++ java/1035.java
@@ -0,0 +1,38 @@
+package iniciante;
+
+/*
+
+ QUEST�O
+ Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C 
+ e se D for maior do que A, e a soma de C com D for maior que a soma 
+ de A e B e se C e D, ambos, forem positivos e se a vari�vel A for par 
+ escrever a mensagem "Valores aceitos", sen�o escrever "Valores nao aceitos".
+
+ ENTRADA
+ Quatro n�meros inteiros A, B, C e D.
+
+ SA�DA
+ Mostre a respectiva mensagem ap�s a valida��o dos valores.
+
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Beecrowd1035 {
+    
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int a = leitor.nextInt();
+        int b = leitor.nextInt();
+        int c = leitor.nextInt();
+        int d = leitor.nextInt();
+        if (b > c && d > a && c > 0 && d > 0 && (c + d) > (a + b) && a % 2 == 0) {
+            System.out.println("Valores aceitos"); 
+        } else { 
+            System.out.println("Valores nao aceitos");
+        }
+        
+    }
+    
+}
