9b2c3d5387ce8720faf0a8322736a31fe876b989
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-04 23:05:40 -0300

    Resolução 1041.java

---
+++ java/1041.java
@@ -0,0 +1,47 @@
+package iniciante;
+
+/*
+ 
+ QUEST�O
+ Leia 2 valores com uma casa decimal (x e y), que devem representar 
+ as coordenadas de um ponto em um plano. A seguir, determine qual o 
+ quadrante ao qual pertence o ponto, ou se est� sobre um dos eixos 
+ cartesianos ou na origem (x = y = 0).
+ Se o ponto estiver na origem, escreva a mensagem �Origem�.
+ Se o ponto estiver sobre um dos eixos escreva �Eixo X� ou �Eixo Y�, conforme for a situa��o.
+ 
+ ENTRADA
+ A entrada contem as coordenadas de um ponto.
+
+ SA�DA 
+ A sa�da deve apresentar o quadrante em que o ponto se encontra.
+
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Beecrowd1041 {
+    
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        double x = leitor.nextDouble();
+        double y = leitor.nextDouble();
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        } else if (x == 0) {
+            System.out.println("Eixo Y");
+        } else if (y == 0) {
+            System.out.println("Eixo X");
+        } else if (y > 0 && x > 0) {
+            System.out.println("Q1");
+        } else if (y > 0 && x < 0) {
+            System.out.println("Q2");
+        } else if (y < 0 && x < 0) {
+            System.out.println("Q3");
+        } else if (y < 0 && x > 0) {
+            System.out.println("Q4");
+        }
+    }
+    
+}
